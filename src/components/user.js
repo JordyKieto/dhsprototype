89438d190bdd51d48df7f85d7e9b971c987c2800
unknown <kieto@ualbert.ca>
2019-03-19 08:20:25 -0400

    mock fetch

--- src/components/user.js
+++ src/components/user.js
@@ -3,20 +3,20 @@ import React from 'react';
 const user = async ({firstName, lastName, age, phoneNumber, familySize, dependant, education})=>{
     const url = '/api/users'
     const data = {firstName, lastName, age, phoneNumber, familySize, dependant, education};
-    fetch(url, {
-        method: "POST", 
-        mode: "cors", 
-        cache: "no-cache", 
-        credentials: "same-origin", 
-        headers: {
-            "Content-Type": "application/json",
-        },
-        redirect: "follow", 
-        referrer: "no-referrer", 
-        body: JSON.stringify(data),
-    }).then(res=>{
-        res.json();
-        console.log(res);
+    // fetch(url, {
+    //     method: "POST", 
+    //     mode: "cors", 
+    //     cache: "no-cache", 
+    //     credentials: "same-origin", 
+    //     headers: {
+    //         "Content-Type": "application/json",
+    //     },
+    //     redirect: "follow", 
+    //     referrer: "no-referrer", 
+    //     body: JSON.stringify(data),
+    // }).then(res=>{
+    //     res.json();
+    //     console.log(res);
         return(
             <>
                 <p>Hello there {firstName} {lastName} </p>
@@ -27,7 +27,7 @@ const user = async ({firstName, lastName, age, phoneNumber, familySize, dependan
                 <p>{education} is the highest level of education you have completed</p>
             </>
         );
-    })
+    // })
 }
 
 export default user;
